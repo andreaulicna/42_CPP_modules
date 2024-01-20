a52d5c03b3f34c721a97d913b4d28b6753dc0763
aulicna <andrea@ulicna.com>
2024-01-20 15:28:54 +0100

    CPP 01: ex04 done. Now replaces all the occurrence (even within a word) and is documented. Tests to be added.

--- CPP_Module_01/ex04/main.cpp
+++ CPP_Module_01/ex04/main.cpp
@@ -6,33 +6,31 @@
 /*   By: aulicna <aulicna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 20:43:30 by aulicna           #+#    #+#             */
-/*   Updated: 2024/01/20 13:07:19 by aulicna          ###   ########.fr       */
+/*   Updated: 2024/01/20 15:25:39 by aulicna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+ * @file	main.cpp
+ * @brief	A program to replace occurrences of a string in a file with another
+ * string.
+ *
+ * This program takes three parameters: a filename and two strings, s1 and s2.
+ * It opens the file specified by the filename, reads its contents, and replaces
+ * every occurrence of s1 with s2. The modified content is then written
+ * to a new file named <filename>.replace.
+ */
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 
-void	extractStringFromLine(std::string line, std::ofstream &ofs,
-	std::string s1, std::string s2)
-{
-	std::stringstream	ss;
-	std::string			str;
-	
-	ss.str(line); // Set the new content
-	while (std::getline(ss, str, ' '))
-	{
-		if (str == s1)
-			ofs << s2 << ' ';
-		else
-			ofs << str << ' ';
-	}
-	ss.clear(); // Clear any error flags
-	ss.str(""); // Clear the content
-}
-
+/**
+ * @function	wrongParameters
+ * @brief	Handles the wrong numberof parameters provided.
+ * @return 	int 	returns 1 to indicate an error.
+ */
 int	wrongParameters(void)
 {
 	std::cout << "Error: Wrong number of parameters provided.\n"
@@ -40,37 +38,72 @@ int	wrongParameters(void)
 	return (1);
 }
 
+/**
+ * @function	errorOpenFile
+ * @brief	Prints an error message and returns 1 when a file fails to open.
+ * @param	name	name of the file that failed to open
+ * @return	int		returns 1 to indicate an error
+ */
 int	errorOpenFile(std::string name)
 {
 	std::cout << "Error: "<< name << " file open error." << std::endl;
 	return (1);
 }
 
+/**
+ * @function	copyContentIntoReplace
+ * @brief	Copies the content of the original file into a new file,
+ * replacing occurrences of s1 with s2.
+ * @param	ofs				ofstream object for the new file
+ * @param	originalContent	content of the original file
+ * @param	s1				string to be replaced
+ * @param	s2				string to replace s1
+ * @return	int				returns 0 on success, 1 on failure
+ */
+int	copyContentIntoReplace(std::ofstream &ofs, std::string originalContent,
+	std::string s1, std::string s2)
+{
+	std::size_t			i;
+	std::size_t			move_to;	
+
+	i = 0;
+	while (true)
+	{
+		move_to = originalContent.find(s1, i);
+		if (move_to != std::string::npos)
+		{
+			ofs << originalContent.substr(i, move_to - i);
+			ofs << s2;
+			i = move_to + s1.length();
+		}
+		else
+		{
+			originalContent.substr(i, std::string::npos);
+			break ;
+		}
+	}
+	return(0);
+}
+
 int	main(int argc, char **argv)
 {
-	std::ifstream	ifs;
-	std::ofstream	ofs;
-	std::string		line;
+	std::ifstream		ifs;
+	std::ofstream		ofs;
+	std::stringstream	buff;
+	std::string			originalContent;
 
 	if (argc < 4)
 		return (wrongParameters());
 	ifs.open(argv[1]);
 	if (!ifs.is_open())
 		return (errorOpenFile("Input"));
+	buff << ifs.rdbuf();
+	ifs.close();
+	originalContent = buff.str();
 	ofs.open(std::string(argv[1]) + ".replace");
 	if (!ofs.is_open())
-	{
-		ifs.close();
 		return (errorOpenFile("Output"));
-	}
-	if (std::getline(ifs, line))
-		extractStringFromLine(line, ofs, argv[2], argv[3]);
-	while (std::getline(ifs, line))
-	{
-		ofs << '\n';
-		extractStringFromLine(line, ofs, argv[2], argv[3]);
-	}
-	ifs.close();
+	copyContentIntoReplace(ofs, originalContent, argv[2], argv[3]);
 	ofs.close();
 	return (0);
 }
